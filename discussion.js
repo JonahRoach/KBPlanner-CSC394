0289054305f01fed53a94de706b7dc343dc9a96c
phrank <phrank@lpc-depaulsecure-201-63.depaulsecure-student.depaul.edu>
2019-11-12 15:28:42 -0600

    added generate post and fixed disussion sections

--- discussion.js
+++ discussion.js
@@ -71,25 +71,19 @@ var sections = ["General", "Brainstorming"]
 
 
 
-var original = document.getElementById("discussion-card")
 
 
-parentEl = document.getElementById('discussion-board');
-
-//this is just some temp code that hides the posts when you switch tabs, doesnt actually work
-$(document).on('shown.bs.tab', function (e) { if(x == 1) {$("#d-content").hide(); x--}else{ $("#d-content").show(); x++ } });
 
 window.onload = function startup()
 {
     populateDiscussionSections()
-    loadPosts()
+    loadPosts("General")
 }
 
 function populateDiscussionSections()
 {
     var parent = document.getElementById("d-navbar")
-    var firstEl = true
-    var a = "active"
+    var discussionBoard = document.getElementById("d-content")
     for(var i = 0; i < sections.length; i++)
     {
         var el = document.createElement('li')
@@ -98,14 +92,20 @@ function populateDiscussionSections()
         el.setAttribute("class", "nav-item")
         if(i == 0)
         {
-            el.innerHTML = "<a class='nav-link active' href='#"+sections[i]+"' data-toggle='tab'>"+ sections[i] +"</a>"
+            board.setAttribute("id", sections[i])
+            board.className = "tab-pane fade in active"
+            el.innerHTML = "<a class='nav-link' href='#"+sections[i]+"' data-toggle='tab'>"+ sections[i] +"</a>"
             parent.appendChild(el)
+            discussionBoard.appendChild(board)
             sectionlen++
         }
         else
         {
+            board.setAttribute("id", sections[i])
+            board.className = "tab-pane fade"
             el.innerHTML = "<a class='nav-link' href='#"+sections[i]+"' data-toggle='tab'>"+ sections[i] +"</a>"
             parent.appendChild(el)
+            discussionBoard.appendChild(board)
             sectionlen++
         }
    
@@ -121,7 +121,7 @@ function loadPostsFromDB()
 {
 
 }
-function loadPosts()
+function loadPosts(divSection)
 {
     for ( var i = 0; i < dposts["posts"].length ; i++)
     {
@@ -140,7 +140,7 @@ function loadPosts()
         childEl.setAttribute("id", name)
         childEl.innerHTML = "<div id="+ name + "' > <div class='card-header' style='padding-top: 20px; outline:soild;'> <img class='card-img-top' src='assets/png/"+dposts.posts[i].rank+".png' alt='Card image' style='width: 3%;'> "+ dposts.posts[i].author + " <div class='card-img text-center'> <h1 style='text-decoration:underline;'>" + dposts.posts[i].title + "</h1> <div class='content'><p>" + dposts.posts[i]["post-detail"] + " </p></div> </div> </div> <div> <small></small> <img alt='' id = 'thumb"+j+"'class='ml-auto' src='assets/svg/thumbs-up.png' style='max-height:2%; max-width:2%;' onclick='changeThumbImage( "+j+");  ' style='width: 2%; padding-left:9px; '> <img id='xmark" + j + "' onclick ='changeXImage("+j+")'src='assets/png/xmark.png' style='max-height:3%; max-width:2.5%;'><img id='qmark" + j + "' onclick ='changeQImage("+j+")'src='assets/png/qmark.png' style='max-height:3%; max-width:3%;'><details style='padding-left:9px; text-decoration:underline;'><summary style='text-decoration:underline;'> Comments </summary><ul id='commentContent" + j+ "'>" + comments +"</ul> <div ><input id='comment-input" + j + "' type='text' placeholder='Enter Comment'><button id='commentButton' type='button' class='btn btn-primary' onclick='addComment("+j+")'> + </button></div></ul> </details></div> </div> </div> </div>"
         console.log(childEl)
-        document.getElementById("d-content").appendChild(childEl);
+        document.getElementById(divSection).appendChild(childEl);
         j = j + 1
    
          
@@ -188,25 +188,51 @@ function changeQImage(id)
 function newPost()
 //code to add a new post
 {
-    var childEl = document.createElement("card"); //creates a new card
+    let childEl = document.createElement("card"); //creates a new card
     var name = "discussion-card"+j
+    var section = getSectionName()
     //details tag not supported by IE
     childEl.setAttribute("class", "card mb-2")
     childEl.setAttribute("id", name)
+    //childEl.appendChild(cardBody)
     childEl.innerHTML = "<div id='discussion-card"+j + "' > <div class='card-header' style='padding-top: 20px; outline:soild;'> <img class='card-img-top' src='assets/png/bronze.png' alt='Card image' style='width: 3%;'> " + userId + " <div class='card-img text-center'> <h1 style='text-decoration:underline;'>" + getPostTitle() + "</h1> <div class='content'><p>" +getPostDetail() + " </p></div> </div> </div> <div> <img alt='' id = 'thumb"+j+"'class='ml-auto' src='assets/svg/thumbs-up.png' style='max-height:2%; max-width:2%;' onclick='changeThumbImage( "+j+") ' style='width: 2%; padding-left:9px; ' id='thumb'> <img id='xmark" + j + "' onclick ='changeXImage("+j+")'src='assets/png/xmark.png' style='max-height:3%; max-width:2.5%;'><img id='qmark" + j + "' onclick ='changeQImage("+j+")'src='assets/png/qmark.png' style='max-height:3%; max-width:3%;'><details style='padding-left:9px; text-decoration:underline;'><summary>Comments </summary><ul id='commentContent" + j+ "'></ul> <div><input id='comment-input" + j + "' type='text' placeholder='Enter Comment'><button id='commentButton' type='button' class='btn btn-primary' onclick='addComment("+j+")'> + </button></div></ul> </details></div> <button onclick='removePost("+j+")'>Remove Post</button> </div> </div> </div>"
     dposts.posts.push({"postId": j, "author": userId, "usersId":userId,"title": getPostTitle(),"post-detail":getPostDetail(),"likes":0,"comments":[]})
     console.log(dposts)
-    document.getElementById("d-content").appendChild(childEl);
+    document.getElementById(section).appendChild(childEl);
     alert("You just recieved 50 points for making a new post!")
     j = j + 1
 }
-
+function generatePost(divSection,userName,postTitle,postDetail,postId,comments)
+{
+    let childEl = document.createElement("card"); //creates a new card
+    var name = "discussion-card"+j
+    //details tag not supported by IE
+    childEl.setAttribute("class", "card mb-2")
+    childEl.setAttribute("id", postId)
+    childEl.innerHTML = 
+        "<div id='"
+          + name +
+        "' > <div class='card-header' style='padding-top: 20px; outline:soild;'> <img class='card-img-top' src='assets/png/bronze.png' alt='Card image' style='width: 3%;'> " 
+        + userName + 
+        " <div class='card-img text-center'> <h1 style='text-decoration:underline;'>" 
+        + postTitle + 
+        "</h1> <div class='content'><p>" 
+        + postDetail + 
+        " </p></div> </div> </div> <div> <img alt='' id = 'thumb"+j+"'class='ml-auto' src='assets/svg/thumbs-up.png' style='max-height:2%; max-width:2%;' onclick='changeThumbImage( "+j+") ' style='width: 2%; padding-left:9px; ' id='thumb'> <img id='xmark" + j + "' onclick ='changeXImage("+j+")'src='assets/png/xmark.png' style='max-height:3%; max-width:2.5%;'><img id='qmark" + j + "' onclick ='changeQImage("+j+")'src='assets/png/qmark.png' style='max-height:3%; max-width:3%;'><details style='padding-left:9px; text-decoration:underline;'><summary>Comments </summary><ul id='commentContent" + j+ "'></ul> <div><input id='comment-input" + j + "' type='text' placeholder='Enter Comment'><button id='commentButton' type='button' class='btn btn-primary' onclick='addComment("+j+")'> + </button></div></ul> </details></div> <button onclick='removePost("+j+")'>Remove Post</button> </div> </div> </div>"
+    dposts.posts.push({"postId": j, "author": userId, "usersId":userId,"title": getPostTitle(),"post-detail":getPostDetail(),"likes":0,"comments":[]})
+    console.log(dposts)
+    document.getElementById(divSection).appendChild(childEl);
+    alert("You just recieved 50 points for making a new post!")
+    j = j + 1
+}
 function addDiscussionSection()
 {
 
     var section = document.getElementById("section").value;
     var parent = document.getElementById("d-navbar")
     var el = document.createElement('li')
+    var discussionBoard = document.getElementById("d-content")
+    var board = document.createElement("div")
     el.setAttribute("class", "nav-item")
     if(sectionlen >= maxSectionLen)
     {
@@ -220,11 +246,14 @@ function addDiscussionSection()
     }
     else
     {
-    el.innerHTML = "<a class='nav-link'  href='#"+section+"' data-toggle='tab'>"+ section +"</a>"
-    sections.push(section)
-    sectionlen++
-    parent.appendChild(el)
-    console.log(sectionlen)
+        board.setAttribute("id", section)
+        board.className = "tab-pane fade"
+        el.innerHTML = "<a class='nav-link'  href='#"+section+"' data-toggle='tab'>"+ section +"</a>"
+        sections.push(section)
+        sectionlen++
+        parent.appendChild(el)
+        discussionBoard.appendChild(board)
+        console.log(sectionlen)
     }
 }
 function addComment(id)
@@ -258,6 +287,12 @@ function clearFormInput()
 {
     document.getElementById("title-post").value= "";
     document.getElementById("post-detail").value="";
+    document.getElementById("section-post").value="";
+
+}
+function getSectionName()
+{
+    return document.getElementById("section-post").value 
 
 }
 function getPostTitle()
